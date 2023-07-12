582ad667ea86e1284904dd0776b7ae44a8dac6e5
Quinn Le <quynhnle135@gmail.com>
2023-07-12 15:43:11 -0400

    Did Valid Palindrome exercise

---
+++ two_pointers/main.py
@@ -0,0 +1,12 @@
+from valid_palindrome import check_valid_palindrome
+
+
+def main():
+    print(check_valid_palindrome("abba"))
+    print(check_valid_palindrome("kayak"))
+    print(check_valid_palindrome("hannah"))
+    print(check_valid_palindrome("123454321"))
+
+
+if __name__ == "__main__":
+    main()
--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -1,31 +1,10 @@
-def validPalindrome(s):
-    left = 0
-    right = len(s) - 1
-    while left <= right:
-        if s[left] != s[right]:
+def check_valid_palindrome(input):
+    start = 0
+    end = len(input) - 1
+    while start < end:
+        if input[start] != input[end]:
             return False
-        else:
-            left += 1
-            right -= 1
+        start += 1
+        end -= 1
     return True
 
-
-def main():
-    test_cases = ["RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"]
-    for i in range(len(test_cases)):
-        print("Test case #", i +1)
-        print("-" * 100)
-        print("\tThe input string is ", test_cases[i], "ang the length of the string is ", len(test_cases[i]))
-        print("\nIs it a palindrome? ", validPalindrome(test_cases[i]))
-        print("-" * 100)
-
-
-if __name__ == "__main__":
-    main()
-
-
-# s1 = "abba"
-# print(validPalindrome(s1))
-#
-# s2 = "abcd"
-# print(validPalindrome(s2))
